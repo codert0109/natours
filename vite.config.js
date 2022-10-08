d95b7326af9da616b663efcf05b72ac2d1ce989c
codert0109 <thebluemoonsea@gmail.com>
2022-10-08 12:47:11 +0530

    Update vite config with rewrite rule in proxy for api

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const isDev = mode === 'development';
+  const serverUrl = isDev ? 'http://localhost:3000' : 'https://natours-arun.herokuapp.com';
 
   return {
     plugins: [react()],
     server: {
       proxy: {
         '/api/v1': {
-          target: isDev ? 'http://localhost:3000' : 'https://natours-arun.herokuapp.com',
+          target: serverUrl,
           changeOrigin: true,
           secure: false,
           ws: true,
+          rewrite: path => path.replace(/^\/api\/v1/, `${serverUrl}/api/v1`),
         },
       },
     },
