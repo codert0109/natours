f6338a64b79bf87a7438bf86f3c8a29d76fb165e
codert0109 <thebluemoonsea@gmail.com>
2022-12-27 01:19:56 +0530

    Change the production url for backend service

--- src/constants/serverConstants.js
+++ src/constants/serverConstants.js
@@ -1,4 +1,4 @@
 export const SERVER_BASE_URL =
   import.meta.env.MODE === 'production'
-    ? 'https://natours-backend-production.up.railway.app/'
+    ? 'https://natours-backend-production.up.railway.app'
     : 'http://localhost:3000';
