761828369cbf42c3000b0d5204386b8c09037239
codert0109 <thebluemoonsea@gmail.com>
2022-12-27 01:17:36 +0530

    Change the production url for backend service

--- src/constants/serverConstants.js
+++ src/constants/serverConstants.js
@@ -1,4 +1,4 @@
 export const SERVER_BASE_URL =
   import.meta.env.MODE === 'production'
-    ? 'https://natours-arun.herokuapp.com'
+    ? 'https://natours-backend-production.up.railway.app/'
     : 'http://localhost:3000';
